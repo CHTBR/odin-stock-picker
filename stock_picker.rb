eff2477db47d6381fb22f245897d7e28f502e8a2
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-07-04 11:45:17 +0200

    Find values and days of highest and lowest stocks

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,7 +1,10 @@
 def stock_picker(stock_arr)
   mod_arr = stock_arr
-  highest_price = 0
+  highest_price = {:value = 0, :buy_day = nil, :sell_day = nil}
   while mod_arr.max > highest_price || mod_arr.size < 2
+
+    highest_stock = {value: mod_arr.max}; highest_stock[:day] = mod_arr.day(highest_stock[:vale])
+    lowest_stock = {value: mod_arr[0, highest_stock[:day]].min}; lowest_stock[:day] = mod_arr.day(lowest_stock[:value])
     
   end
 end
