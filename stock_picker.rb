7640c0a96f0afb8711486e0de60b9f2fb1c986ea
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-07-04 12:07:01 +0200

    Fix syntax errors and shorten array after each loop Fix assignment in hash highest_price, searching for indexes of stocks

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,10 +1,10 @@
 def stock_picker(stock_arr)
   mod_arr = stock_arr
-  highest_price = {:value = 0, :buy_day = nil, :sell_day = nil}
-  while mod_arr.max > highest_price || mod_arr.size < 2
+  highest_price = {value: 0, buy_day: nil, sell_day: nil}
+  while mod_arr.max > highest_price[:value] || mod_arr.size < 2
 
-    highest_stock = {value: mod_arr.max}; highest_stock[:day] = mod_arr.day(highest_stock[:vale])
-    lowest_stock = {value: mod_arr[0, highest_stock[:day]].min}; lowest_stock[:day] = mod_arr.day(lowest_stock[:value])
+    highest_stock = {value: mod_arr.max}; highest_stock[:day] = mod_arr.index(highest_stock[:value])
+    lowest_stock = {value: mod_arr[0, highest_stock[:day]].min}; lowest_stock[:day] = mod_arr.index(lowest_stock[:value])
 
     unless lowest_stock[:day] == nil
       price = highest_stock[:value] - lowest_stock[:value]
@@ -18,5 +18,8 @@ def stock_picker(stock_arr)
       highest_price[:sell_day] = highest_stock[:day]
     end
 
+    mod_arr = mod_arr[highest_stock[:day]+1, mod_arr.size]
+
   end
+  highest_price
 end
